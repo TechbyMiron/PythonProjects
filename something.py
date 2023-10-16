2d777ea8a8330afed62ac914d8b355d2317fd84d
TechbyMiron <mironsimpson00@gmail.com>
2023-10-15 22:02:05 -0500

    Intial comment

---
+++ something.py
@@ -0,0 +1,23 @@
+import random
+# create a game similar like FizzBuzz
+
+target = 100
+random.jungle = random.randint(1, target +1)
+
+
+print("Welcome to the JungleJack project")
+def jungle_jack():
+         pass
+
+for number in range(100):
+  if number % 5 == 0 and number % 7 == 0:
+     print("JungleJack")
+  elif number % 5 == 0:
+     print("Jungle")
+  elif number % 7 == 0:
+     print("Jack")
+  elif number % 1 == 0 and number % 4 == 0:
+     print("JungleJack")
+  else:
+     print(number)
+
