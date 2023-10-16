905e780f56d5cb2972b742281c8e37e87f90207b
TechbyMiron <mironsimpson00@gmail.com>
2023-10-15 21:59:21 -0500

    Intial comment

---
+++ main.py
@@ -0,0 +1,29 @@
+from turtle import Turtle, Screen
+
+kev = Turtle()
+kev.shape("turtle")
+kev.color("pink")
+for _ in range(4):
+    kev.forward(-100)
+    kev.circle(-100)
+    kev.fd(100)
+    kev.right(90)
+    kev.circle(100)
+    kev.backward(10)
+
+
+    def draw_spirogragh(size_of_gap):
+        for _ in range(int(960 / size_of_gap)):
+            kev.speed(11)
+            kev.circle(100)
+        kev.setheading(kev.heading() + size_of_gap)
+
+    draw_spirogragh(12)
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
